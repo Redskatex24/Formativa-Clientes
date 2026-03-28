60cbb46eafcdaac5341ecfeccb6a9afdb0fbcc0e
Redskatex24 <ju.garridog@duocuc.cl>
2026-03-27 23:10:31 -0300

    eliminación de jarkata

--- src/main/java/Formativa/Model/Clientes.java
+++ src/main/java/Formativa/Model/Clientes.java
@@ -3,8 +3,6 @@ package Formativa.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -12,26 +10,13 @@ import jakarta.validation.constraints.NotNull;
 public class Clientes {
 
     private int id;
-
-    @NotBlank(message = "el nombre es obligatorio")
     private String nombre;
-
-    @NotBlank(message = "el rut es obligatorio")
     private String rut;
-
-    @NotBlank(message = "la edad es obligatoria")
     private String edad;
-
-    @NotNull(message = "el email es obligatorio")
     private String email;
     private String telefono;
     private String direccion;
     private String ciudad;
     private String pais;
-
-    public String getNombre() {return nombre;}
-    public String getRut() {return rut;}
-    public String getEdad() {return edad;}
-    public String getEmail() {return email;}
 
 }
